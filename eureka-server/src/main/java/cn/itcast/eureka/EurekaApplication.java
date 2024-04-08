8f95d4b1677ddc454432c997037cbdd7c1c5bfed
dongchao <kevin@qq.com>
2024-04-08 18:21:28 +0800

    first commit

--- eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -1,5 +1,6 @@
 package cn.itcast.eureka;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,6 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaApplication.class,args);
     }
 }
